e588c226dd0c691bb9076d95c656d0ad602bff6a
vishwajeet singh <v.k.singh220701@gmail.com>
2021-06-30 10:37:13 +0530

    added navbar and total

--- src/App.js
+++ src/App.js
@@ -1,12 +1,103 @@
 import React from 'react'
 import Cart from './Cart';
+import Navbar from './Navbar'
+class App extends React.Component{
 
-function App() {
-  return (
-    <div className="App"> 
-      <Cart/>
-    </div>
-  );
+  constructor(){
+    super(); // calls constructor of the parent class. 
+    this.state={
+        products: [
+           { 
+            price:999,
+            title:'Watch',
+            qty: 1,
+            img:'https://images.unsplash.com/photo-1542496658-e33a6d0d50f6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+            id :1
+           },
+           { 
+            price:6700,
+            title:'Phone',
+            qty: 1,
+            img:'https://images.unsplash.com/photo-1534536281715-e28d76689b4d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+            id :2
+           },
+           { 
+            price:5000,
+            title:'Laptop',
+            qty: 1,
+            img:'https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2089&q=80',
+            id: 3   
+           }
+        ]
+    }
 }
+    handleIncreaseQuantity= (product) => {
+        console.log();
+        const {products}=this.state;
+        const index=products.indexOf(product);
+        
+        products[index].qty += 1;
 
+        this.setState({
+            products
+        })
+    }
+    
+    handleDecreaseQuantity= (product) => {
+        console.log();
+        const {products}=this.state;
+        const index=products.indexOf(product);
+        
+        if (products[index].qty === 0){
+            return;
+        }
+        products[index].qty -= 1;
+
+        this.setState({
+            products
+        })
+    }
+    handleDeleteProduct = (id) =>{
+        const{products}= this.state;
+        const items=products.filter((item) => item.id !==id);
+        this.setState({
+            products: items
+        })
+    }
+    getCartCount=() =>{
+      const{products}=this.state;
+      let count=0;
+      products.forEach((product) =>{
+        count +=product.qty;
+      })
+
+      return count;
+    }
+    getCartTotal =() =>{
+      const{products}=this.state;
+
+      let cartTotal=0;
+      products.map((product) => {
+        cartTotal =cartTotal + product.qty * product.price
+      })
+
+      return cartTotal;
+    }
+
+  render(){
+    const{products}=this.state;
+  return (
+      <div className="App"> 
+      <Navbar count={this.getCartCount()} />
+        <Cart
+        products={products}
+        onIncreaseQuantity={this.handleIncreaseQuantity}
+        onDecreaseQuantity={this.handleDecreaseQuantity}
+        onDeleteProduct={this.handleDeleteProduct}
+        />
+        <div style={{padding: 10,fontSize:20}}>TOTAL:{this.getCartTotal()}</div>
+      </div>
+   );
+  }
+}
 export default App;
--- src/Cart.js
+++ src/Cart.js
@@ -1,17 +1,27 @@
 
 import React from 'react';
 import CartItem from './CartItem';
-class Cart extends React.Component{
-    render(){
-        return(
-            <div className="cart">
-                <CartItem/>
-                <CartItem/>
-                <CartItem/>
-            </div>
-        );
-    }
+const Cart=(props)=>{
+    
+    
+    const{products}=props;
+    return(
+        <div className="cart">
+            {products.map((product) => {
+                return (
+                <CartItem 
+                product={product} 
+                key={product.id}
+                onIncreaseQuantity={props.onIncreaseQuantity}
+                onDecreaseQuantity={props.onDecreaseQuantity}
+                onDeleteProduct={props.onDeleteProduct}
+                />
+                )
+            })}
+        </div>
+    );
+    
 
-}
+}   
 
 export default Cart;    
